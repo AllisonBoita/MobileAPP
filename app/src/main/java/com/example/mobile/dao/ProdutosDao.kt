89c8af641d7b0cf5b89e53529e09ddd77238e1df
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-04-08 08:46:44 -0300

    Adicionando imagens para diferentes resoluções

--- app/src/main/java/com/example/mobile/dao/ProdutosDao.kt
+++ app/src/main/java/com/example/mobile/dao/ProdutosDao.kt
@@ -1,19 +1,31 @@
 package com.example.mobile.dao
 
 import com.example.mobile.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
-    fun adiciona(produto: Produto){
+    fun adiciona(produto: Produto) {
         produtos.add(produto)
     }
 
-    fun buscaTodos() : List<Produto> {
+    fun buscaTodos(): List<Produto> {
         return produtos.toList()
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Cesta de Frutas",
+                descricao = "Laranjas, maças e uvas",
+                valor = BigDecimal("10.93"),
+                disponivel = "Sim"
+            ),
+            Produto(nome = "Pack de bebida",
+                descricao = "Large Beer",
+                valor = BigDecimal("19.90"),
+                disponivel = "Não")
+        )
     }
 
 }
