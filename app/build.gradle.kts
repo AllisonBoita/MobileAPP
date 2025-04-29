cfcf75d9ebd0d48020b77af413988c9739432366
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-04-29 13:15:48 -0300

    Atualizando KAPT para KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("kotlin-parcelize")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -47,7 +48,7 @@ dependencies {
     val room_version = "2.7.1"
 
     implementation("androidx.room:room-runtime:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
+    ksp("androidx.room:room-compiler:2.7.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation ("org.jetbrains.kotlin:kotlin-stdlib:2.1.20")
