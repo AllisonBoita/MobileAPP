6fbc7ad19d6fa983596ab988d77dc6cbe5601442
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-05-02 08:13:59 -0300

    Alterando implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     val room_version = "2.7.1"
 
     implementation("androidx.room:room-runtime:$room_version")
-    ksp("androidx.room:room-compiler:2.7.1")
+    kapt ("androidx.room:room-compiler:$room_version")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation ("org.jetbrains.kotlin:kotlin-stdlib:2.1.20")
---
+++ app/src/main/java/com/example/mobile/database/dao/AppDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.mobile.database.dao
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.mobile.model.Produto
+
+@Database(entities = [Produto::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun produtoDao() : ProdutoDao
+}
